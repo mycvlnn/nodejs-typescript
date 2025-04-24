627b4db1ea55fecccb79c457d32e0db27174a32b
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-04-24 10:51:10 +0700

    feat: config commit lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 export default defineConfig([
   { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
   {
     plugins: {
